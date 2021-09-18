184452c7edee283db5d831031408b4728075512b
Sepyk <sepyk93@gmail.com>
2021-09-18 15:50:45 +0300

    FireBase integration

--- Instagram/Controllers/Core Tabs/HomeViewController.swift
+++ Instagram/Controllers/Core Tabs/HomeViewController.swift
@@ -5,15 +5,29 @@
 //  Created by Остап Сепик on 18.09.2021.
 //
 
+import FirebaseAuth
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        handleAreNotIdentificated()
+    }
+    
+    private func handleAreNotIdentificated() {
+        //Check auth status
+        if Auth.auth().currentUser == nil{
+        //show Log In
+        let loginVC = LoginViewController()
+            loginVC.modalPresentationStyle = .fullScreen
+            present(loginVC, animated: false)
+        }
+        
+    }
+    
 }
 
--- Instagram/Controllers/Other/Onboarding/LoginViewController.swift
+++ Instagram/Controllers/Other/Onboarding/LoginViewController.swift
@@ -12,7 +12,7 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
     
  
